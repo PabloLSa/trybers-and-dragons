285ee0914d20d129c2901acb98be73337e809334
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-06-12 15:51:53 -0300

    Start
    Starting project

--- src/utils.ts
+++ src/utils.ts
@@ -3,5 +3,5 @@ function getRandomInt(min: number, max: number) {
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
 }
-
+// start 
 export default getRandomInt;
