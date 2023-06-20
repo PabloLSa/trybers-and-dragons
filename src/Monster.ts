a4528f2d19e5510ac1e82d8d45f9b12c7cc8f541
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-06-20 15:06:38 -0300

    :sparkles: Class Monster
     - Criei classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import SimpleFighter from './Fighter/SimpleFighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (this.lifePoints - attackPoints < 1) {
+      this._lifePoints = -1;
+      return this._lifePoints;
+    }
+    this._lifePoints -= attackPoints;
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): number {
+    return enemy.receiveDamage(this.strength);
+  }
+}
